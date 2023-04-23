16fe2ade37a16d6fd4cedaf3f86bd6d0f6b9a189
Oded Masala <odedm@moveo.co.il>
2023-04-23 09:53:17 +0300

    finsh bag and popup dish

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,6 @@ export { default as Card , DisplayStars} from "./card/Card";
 export { default as BagShop } from "./bag/BagShop";
 export { default as InputSearch } from "./inputSearch/InputSearch";
 export { default as ClickButton } from "./buttons/clickButton/ClickButton";
-
+export { default as PopUp } from "./popUp/PopUp";
+export { default as DishComponent } from "./dish/Dish";
 export { DistanceComponent,PriceComponent,RatingComponent } from "./range/RangeComponents";
