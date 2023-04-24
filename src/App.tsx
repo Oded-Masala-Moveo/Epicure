bc57ae447b6aeaec2d1b434dda2571a4b5ee6cf6
Oded Masala <odedm@moveo.co.il>
2023-04-25 01:39:36 +0300

    create checkout file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from "./layouts";
-import { RestaurantsPage, HomePage, RestaurantPage, ChefPage } from "./pages";
+import { RestaurantsPage, HomePage, RestaurantPage, ChefPage, CheckOut } from "./pages";
 
 import "./app.scss";
 import { selectBag, useAppSelector } from "./store";
@@ -20,6 +20,7 @@ console.log(cart);
           <Route path="/restaurants" element={<RestaurantsPage />} />
           <Route path="/restaurants/:restId" element={<RestaurantPage />} />
           <Route path="/chef" element={<ChefPage />} />
+          <Route path="/checkout" element={< CheckOut />} />
         </Routes>
       <Footer />
     </div>
---
+++ src/pages/checkout/CheckOut.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./checkout.scss"
+const CheckOut: React.FC = () => {
+  return (
+    <>
+      <div className="checkout-page-container">CheckOut</div>
+    </>
+  );
+};
+
+export default CheckOut;
