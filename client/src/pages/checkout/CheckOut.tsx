b7466aa7c251ab87845eaab0a34204e0db24870c
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-04-27 15:05:21 +0300

    fix bugs in mobile size

--- client/src/pages/checkout/CheckOut.tsx
+++ client/src/pages/checkout/CheckOut.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import "./checkout.scss";
 import { Logo, X_dark } from "../../assets/icons";
-import { CheckOutForm } from "../../components";
+import { CheckOutForm, ClickButton } from "../../components";
+import {
+  selectBag,
+  selectBagDishes,
+  selectBagTotal,
+  useAppDispatch,
+  useAppSelector,
+} from "../../store";
+import { BagDishCard } from "../../components/bag/bagDishCard/BagDishCard";
 
 const CheckOut: React.FC = () => {
   const refInput = useRef<HTMLInputElement>(null);
+  const currentBagDishes = useAppSelector(selectBagDishes);
+  const currentTotal = useAppSelector(selectBagTotal);
+  const dispatch = useAppDispatch();
   useEffect(() => {
-    console.log(refInput);
+    console.log(currentBagDishes);
   }, []);
   return (
     <>
@@ -24,6 +35,21 @@ const CheckOut: React.FC = () => {
         </div>
         <div className="checkout-form-container">
           <CheckOutForm />
+        </div>
+        <div>
+          <h2>My order</h2>
+          <div className="order-details-container">
+            {currentBagDishes.length > 0 &&
+              currentBagDishes.map((dish) => <BagDishCard item={dish}/>)}
+          </div>
+          <div className="order-total-container">
+            <p>TOTAL - {currentTotal}</p>
+          </div>
+          <div className="submit-button-container">
+            <ClickButton icon={true}  width="335px">
+            Complete payment
+            </ClickButton>
+          </div>
         </div>
       </div>
     </>
