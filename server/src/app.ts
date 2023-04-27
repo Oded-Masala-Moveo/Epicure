ac5618c93522f7702eae5ea808a63386c1989883
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-04-27 17:56:36 +0300

    finish file sistem

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,17 @@
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Define your API routes here
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
+  
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
