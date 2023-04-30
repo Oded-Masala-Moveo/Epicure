251c88e7908c668dfcd3d16f929d36b37bc08c1c
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-04-30 14:34:42 +0300

    api handle Error

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,17 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import { ErrorHandler } from "./exceptions";
 import { apiRouter } from "./routes";
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Define your API routes here
+// API routes
 app.use("/api", apiRouter);
 
+// API handle Error in the app
+app.use(ErrorHandler.handleError);
 import "./config/config";
+import "./process";
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
