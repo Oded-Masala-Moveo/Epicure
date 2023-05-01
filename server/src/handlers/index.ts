fbc2a3a505a4e4ed6698b5f04c0ca9a77d39fd98
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-05-01 16:10:28 +0300

    add search route for upcoming future

--- server/src/handlers/index.ts
+++ server/src/handlers/index.ts
@@ -1,3 +1,4 @@
 export { default as ChefHandler } from "./Chef.handler";
 export { default as RestaurantHandler } from "./restaurant.handler";
 export { default as DishHandler } from "./Dishes.handler";
+export { default as SearchHandler } from "./Search.handler";
