dfebc7a6b024c66edf9b044b53a216b0c2337d89
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-05-07 11:06:41 +0300

    finished add order schema

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,3 +3,4 @@ export { default as RestaurantController } from "./restaurant.controller";
 export { default as DishController } from "./dish.controller";
 export { default as SearchController } from "./search.controller";
 export { default as UserController } from "./user.controller";
+export { default as orderController } from "./order.controller";
--- server/src/handlers/index.ts
+++ server/src/handlers/index.ts
@@ -3,3 +3,4 @@ export { default as RestaurantHandler } from "./restaurant.handler";
 export { default as DishHandler } from "./Dishes.handler";
 export { default as SearchHandler } from "./Search.handler";
 export { default as UserHandler } from "./User.handler";
+export { default as OrderHandler } from "./Order.handler";
