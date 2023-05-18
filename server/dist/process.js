66fe824407f26f76d2aab4a2c2e1b58015461d28
Oded-Masala-Moveo <128299215+Oded-Masala-Moveo@users.noreply.github.com>
2023-05-18 09:34:41 +0300

    delete to split server repository

--- server/dist/process.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-process.on('unhandledRejection', (reason) => {
-    console.log(`Unhandled Rejection: ${reason.message || reason}`);
-    throw new Error(reason.message || reason);
-});
-//# sourceMappingURL=process.js.map
